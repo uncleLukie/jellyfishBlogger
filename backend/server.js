3057c188588e1f00e85f32f19f8d9005687f4024
Luke Hewitt <sensator@gmail.com>
2023-04-26 16:15:53 +1000

    fixed the ports so that nginx can see backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });

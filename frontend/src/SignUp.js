b359bd876f48200a466e6a0bbeeff20ac7612c84
UncleLukie <lukehewitt@smashit.games>
2023-04-30 12:10:12 +1000

    SignUp.js fixed!

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebase';
 
 const SignUp = () => {
@@ -9,7 +10,7 @@ const SignUp = () => {
   const signUp = async (e) => {
     e.preventDefault();
     try {
-      const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       console.log('User signed up successfully:', userCredential.user);
       setError(null);
     } catch (error) {

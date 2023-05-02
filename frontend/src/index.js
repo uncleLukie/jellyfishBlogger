9b419e58ed3753cc63c157019b7d2b03f4efb2d3
UncleLukie <lukehewitt@smashit.games>
2023-05-02 11:26:04 +1000

    removing reactbuddy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

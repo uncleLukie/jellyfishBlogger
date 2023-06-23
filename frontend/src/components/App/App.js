82d614ff04d68cd60e3facd69005826319d7466e
UncleLukie <lukehewitt@smashit.games>
2023-06-23 11:22:31 +1000

    Fix Routing Context Error
    Wrapped the application within BrowserRouter to provide routing context. This resolves an error that was occurring when the Link component was being used without the necessary routing context provided by BrowserRouter. Now all routing components should work as expected.

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -5,18 +5,21 @@ import HomePage from '../HomePage/HomePage';
 import theme from '../../theme/theme';
 import { Container } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <HomePage />
-        <Container maxWidth="sm">
-          <SignUp />
-        </Container>
-      </div>
-    </ThemeProvider>
-  );
+    return (
+        <Router>
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <HomePage />
+                    <Container maxWidth="sm">
+                        <SignUp />
+                    </Container>
+                </div>
+            </ThemeProvider>
+        </Router>
+    );
 }
 
 export default App;
--- frontend/src/components/BlogList/BlogList.js
+++ frontend/src/components/BlogList/BlogList.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
-const Footer = () => {
-  return (
-    <footer>
-      <Container maxWidth="sm">
-        <Typography variant="body1" align="center">
-          © Jellyfish Blogger 2023
-        </Typography>
-      </Container>
-    </footer>
-  );
+const BlogList = () => {
+    // For now, use some dummy data to display blog posts
+    const blogPosts = [
+        { id: 1, title: 'First Post', author: 'John Doe', content: 'This is the first blog post.' },
+        { id: 2, title: 'Second Post', author: 'Jane Doe', content: 'This is the second blog post.' },
+        { id: 3, title: 'Third Post', author: 'John Doe', content: 'This is the third blog post.' },
+    ];
+
+    return (
+        <Box sx={{ mt: 4 }}>
+            <Typography variant="h4" gutterBottom>
+                Blog Posts
+            </Typography>
+            {blogPosts.map((post) => (
+                <Box key={post.id} sx={{ mt: 4 }}>
+                    <Typography variant="h5">{post.title}</Typography>
+                    <Typography variant="subtitle1" color="text.secondary">{post.author}</Typography>
+                    <Typography variant="body1">{post.content}</Typography>
+                </Box>
+            ))}
+        </Box>
+    );
 };
 
-export default Footer;
+export default BlogList;

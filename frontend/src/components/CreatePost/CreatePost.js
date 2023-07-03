a019552577f284f720a4cec3534d70307bdb034a
UncleLukie <lukehewitt@smashit.games>
2023-07-03 13:26:02 +1000

    Refactor CreatePost Component for UI enhancements
    changelog:
    - Making use of material ui for fancy form stuff
    - Included the Header component into the CreatePost component
    - Replaced the text input for 'category' with a dropdown selection populated with categories fetched from the API.
    - Adjusted the form to be centred on the page with a wider content text field. (it isn't quite centred yet, idk)

--- frontend/src/components/CreatePost/CreatePost.js
+++ frontend/src/components/CreatePost/CreatePost.js
@@ -1,23 +1,42 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { UserContext } from '../UserContext/UserContext';
+import { TextField, Button, Select, MenuItem, FormControl, InputLabel, Box, Container, Grid } from '@mui/material';
+import Header from '../Header/Header';
 
 const CreatePost = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [category, setCategory] = useState('');
+    const [categories, setCategories] = useState([]);
 
     const user = useContext(UserContext);
 
     const uid = user ? user.uid : null;
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('/api/categories');
+                setCategories(response.data);
+            } catch (error) {
+                console.error(error);
+                alert('Failed to fetch categories');
+            }
+        };
+        fetchCategories();
+    }, []);
 
     const submitPost = async () => {
         if (!uid) {
             alert('User not signed in');
             return;
         }
+        const selectedCategory = categories.find(cat => cat._id === category);
+        const categoryName = selectedCategory ? selectedCategory.name : '';
+
         try {
-            await axios.post('/api/posts', { title, content, author: uid, category });
+            await axios.post('/api/posts', { title, content, author: uid, category: categoryName });
             alert('Post created successfully');
             setTitle('');
             setContent('');
@@ -30,10 +49,38 @@ const CreatePost = () => {
 
     return (
         <div>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
-            <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content"></textarea>
-            <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Category" />
-            <button onClick={submitPost}>Create Post</button>
+            <Header />
+            <Container>
+                <Grid container justifyContent="center">
+                    <Grid item xs={12} md={8}>
+                        <Box component="form" noValidate autoComplete="off" sx={{ '& > :not(style)': { m: 1 }, maxWidth: 500 }}>
+                            <TextField label="Title" value={title} onChange={(e) => setTitle(e.target.value)} fullWidth />
+                            <TextField
+                                label="Content"
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                                multiline
+                                rows={12} // 3x larger content box
+                                variant="outlined"
+                                fullWidth
+                            />
+                            <FormControl fullWidth>
+                                <InputLabel id="category-label">Category</InputLabel>
+                                <Select
+                                    labelId="category-label"
+                                    value={category}
+                                    onChange={(e) => setCategory(e.target.value)}
+                                >
+                                    {categories.map((cat) => (
+                                        <MenuItem key={cat._id} value={cat._id}>{cat.name}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <Button variant="contained" onClick={submitPost}>Create Post</Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
 };
